c51344fdbe56233ca526fab87dd0e8ce10c2119c
Nichole Sanchez <Nichole Sanchez>
2017-05-05 15:28:03 -0600

    Agregar, listar y eliminar jugadores

--- src/lab3_ricardosanchez/Lab3_RicardoSanchez.java
+++ src/lab3_ricardosanchez/Lab3_RicardoSanchez.java
@@ -8,6 +8,11 @@ import java.util.Scanner;
  */
 public class Lab3_RicardoSanchez {
 
+    //Objetos para los jugadores
+    static String Nombre, Apellido, Estado, PaisDNaci, PiePref, Equipo;
+    static int Edad, Num, Precio;
+    //Objetos para los delanteros
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("_____MENU_____"
@@ -16,26 +21,56 @@ public class Lab3_RicardoSanchez {
                 + "\n 3) Salir");
         System.out.println("Ingrese el numero lo que desea ver");
         int Opcion = sc.nextInt();
-        
-        switch(Opcion){
+
+        switch (Opcion) {
             case 1: // JUGADOR ====================================================================================================
                 System.out.println("---Menu Jugador---"
                         + "\n 1) Agregar"
                         + "\n 2) Modificar"
                         + "\n 3) Eliminar");
-                
+                System.out.println("Ingrese el numero del la opcion que desea");
+                int opcion2 = sc.nextInt();
+                switch (opcion2) {
+                    case 1: //Agregar Jugadores------------------------------------------------------------------------------------
+                        char resp;
+                        do {
+                            System.out.println("Agregar Jugador");
+                            System.out.println("Ingrese el primer nombre:");
+                            Nombre = sc.next();
+                            System.out.println("Ingrese apellido");
+                            Apellido = sc.next();
+                            Estado = "Libre";
+                            System.out.println("Ingrese el pasi de nacimiento del jugador");
+                            PaisDNaci = sc.next();
+                            System.out.println("Ingrese el pie preferido");
+                            PiePref = sc.next();
+                            Equipo = "";
+
+                            System.out.println("Desea agregar otro jugador [S/N]");
+                            resp = sc.next().charAt(0);
+                        } while (resp == 's' || resp == 'S');
+                        break;
+                    case 2: //Modificar Jugadores ---------------------------------------------------------------------------------
+
+                        break;
+                    case 3: //Eliminar Jugador ------------------------------------------------------------------------------------
+
+                        break;
+                    default:
+                        break;
+                }
+
                 break;
-            case 2:// EUIPO ========================================================================================================
-                
-                
+            case 2:// EUIPO =======================================================================================================
+
                 break;
             case 3:
                 break;
-                
+
             default:
                 break;
         }
-        
+
     }
-    
+
 }
